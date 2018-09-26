8820886393f4cc9065a7b8145fb28c27ba36bdf3
Quinton Mills <41127568+quintonmills@users.noreply.github.com>
2018-09-26 19:15:55 -0400

    Original code!

---
+++ ranbob.py
@@ -0,0 +1,7 @@
+num = int(input("Please enter a value"))
+for i in range(0,num):
+    for j in range(0,num-i-1):
+        print(end = " ")
+    for j in range(0, i+1):
+        print("$", end = " ")
+    print()
